3a0dd1b524af9d987083b69388c8a902966df9de
jasonz666 <jasonz666@qq.com>
2020-03-11 17:24:36 +0800

    Update tetris.py

--- tetris/tetris.py
+++ tetris/tetris.py
@@ -4,24 +4,31 @@
 # date: 2020-03-08
 # author: Jason
 
+
 # ##########################
 # 终端俄罗斯方块Tetris
 # ##########################
 
+
 import time
+import random
+
 
 # #################
 # 全局变量定义
 # #################
 
+
 # 游戏区域长度高度 1个方块2个英文字符 实际长度要x2
 GAME_AREA_L, GAME_AREA_H = 20, 24
-# 游戏区域左上角起始点坐标 假设此点为坐标原点 X为横坐标 Y为纵坐标
+# 假设游戏区域内左上角坐标为坐标原点 X为横坐标 Y为纵坐标
 GAME_AREA_X, GAME_AREA_Y = 2, 2
 # 游戏边框图形 方块渲染图形
 GAME_EDGE, GAME_SQUARE = '##', 'xx'
 # 游戏区域背景色
-GAME_BACKGROUND = '\033[40;30m'
+GAME_BKG = '\033[40;30m'
+# 清除方块的长度高度
+CLEAR_BLOCK_L, CLEAR_BLOCK_H = 4, 4
 
 # 7方块初始状态位图 全部定义4x4
 # 2,2位置存储方块实际长度高度 3,3位置存储颜色序列
@@ -66,45 +73,33 @@ BLOCK_DICT = {
 }
 
 # 游戏区域 即地图位图
-GAME_BITMAP = [[0 for y in range(GAME_AREA_H)] for x in range(GAME_AREA_L)]
-
+GAME_BITMAP = [[[0, GAME_BKG] for y in range(GAME_AREA_H)] for x in range(GAME_AREA_L)]
 # 单方块临时存储位图
 BLOCK_BITMAP = [[0, 0, [0, 0], 0] if i == 2 else [0, 0, 0, 0] for i in range(4)]
+# 定义方块出生点的方块左上角的地图坐标
+BLOCK_SX, BLOCK_SY = GAME_AREA_X + GAME_AREA_L // 2 - 3, GAME_AREA_Y
+# 方块左上角地图坐标
+BLOCK_COORD = {'x': BLOCK_SX, 'y': BLOCK_SY}
+
 
 # #################
 # 函数定义
 # #################
 
-# TODO 俄罗斯方块未完成。。。。。。。。。。
-# TODO 函数测试
-# tetris_init       OK
-# goto_blockxy      OK
-# clear_blockline   OK
-# clear_area        OK
-# edge_block        OK
-# draw_edge         OK
-# pick_block        OK
-# print_block       OK
-# draw_background   OK
-# restore_cursor    OK
-# rotate_block      OK
-# move_block        ...
-
-# TODO 未完成函数。。。。
-# get_keypress
-#
-
 
 # 初始化与通用函数
 # 包括坐标定位 绘制地图边框 填充地图背景色等函数
 
+
 def goto_blockxy(x=1, y=1):
     """
-    将终端光标移动到方块的起始位置
+    将终端光标移动到方块的起始位置 即坐标定位函数
     :param x: 方块的坐标x
     :param y: 方块的坐标y
     :return: None
     """
+    if x < 1 or y < 1:
+        x, y = 1, 1
     # 1个方块占2个英文字符宽度
     # 实际坐标_x与方块坐标x的转换
     _x = (x - 1) * 2 + 1
@@ -135,7 +130,39 @@ def draw_edge(x=1, y=1, ln=GAME_AREA_L, h=GAME_AREA_H):
     edge_block(x, y + h + 1, ln + 2, 1)
 
 
-def clear_blockline(lenght=1, block=GAME_SQUARE, bkg=GAME_BACKGROUND):
+# 地图处理函数
+# 包括地图中方块清除 地图中方块显示等
+
+
+def clear_map_area(x, y, ln, h):
+    """
+    清除地图位图
+    :param x: 坐标表示法的x
+    :param y: 坐标表示法的y
+    :param ln: 清除区域长度
+    :param h: 清除区域高度
+    :return: 地图位图
+    """
+    map_x, map_y = x - GAME_AREA_X, y - GAME_AREA_Y
+    for _x in range(map_x, map_x + ln):
+        for _y in range(map_y + h):
+            # 坐标表示法与索引表示法互换
+            GAME_BITMAP[map_y][map_x] = [0, GAME_BKG]
+    return GAME_BITMAP
+
+
+def fill_map_area(x, y, color):
+    """填充地图位图
+    :param x: 坐标x
+    :param y: 坐标y
+    :param color: 颜色字符串转义序列
+    """
+    map_x, map_y = x - GAME_AREA_X, y - GAME_AREA_Y
+    GAME_BITMAP[map_y][map_x] = [1, color]
+    return GAME_BITMAP
+
+
+def clear_blockline(lenght=1, block=GAME_SQUARE, bkg=GAME_BKG):
     """
     填充指定长度方块为背景色 即清除方块
     :param lenght: 方块长度 每个方块占2英文字符宽度
@@ -160,11 +187,11 @@ def clear_area(x=GAME_AREA_X, y=GAME_AREA_Y, ln=GAME_AREA_L, h=GAME_AREA_H):
     for _y in range(y, y + h):
         goto_blockxy(x, _y)
         clear_blockline(ln)
+    clear_map_area(x, y, ln, h)
 
 
 def draw_background():
-    """
-    游戏区域背景色填充
+    """游戏区域背景色填充
     """
     clear_area()
 
@@ -190,6 +217,7 @@ def tetris_init():
 # 方块处理函数
 # 包括选取方块 打印方块 方块旋转 方块平移等函数
 
+
 def pick_block(b_type, b_dict=BLOCK_DICT, b_bitmap=BLOCK_BITMAP):
     """
     选取特定类型的方块
@@ -213,6 +241,7 @@ def print_block(x, y, b_bitmap=BLOCK_BITMAP):
     :return: None
     """
     b_l, b_h = BLOCK_BITMAP[-2][-2][0], BLOCK_BITMAP[-2][-2][1]
+    b_color = BLOCK_BITMAP[-1][-1]
     # 这里的_x,_y为索引表示法 参数x,y为坐标表示法
     for _y in range(b_l):
         for _x in range(b_h):
@@ -222,13 +251,14 @@ def print_block(x, y, b_bitmap=BLOCK_BITMAP):
             if b_bitmap[_x][_y] == 1:
                 # 方块位图坐标转换游戏区域坐标 索引到坐标表示法需互换坐标
                 goto_blockxy(_y + x, _x + y)
+                fill_map_area(_y + x, _x + y, b_color)
                 # 打印行数超过终端窗口高度 可能会提前折行导致方块错乱
-                print('{}{}\033[0m'.format(BLOCK_BITMAP[-1][-1], GAME_SQUARE))
+                print('{}{}\033[0m'.format(b_color, GAME_SQUARE))
 
 
 def rotate_block(b_bitmap=BLOCK_BITMAP):
     """
-    逆时针旋转方块 求NxM到MxN的转置矩阵
+    逆时针旋转方块 即求NxM到MxN的转置矩阵
     :param b_bitmap: 方块存储位图 二位列表
     :return: 旋转后的方块位图
     """
@@ -254,7 +284,33 @@ def rotate_block(b_bitmap=BLOCK_BITMAP):
     return b_bitmap
 
 
-def move_block(to_x, to_y, b_bitmap=BLOCK_BITMAP):
+def move_block(x, y, direction, distance=1):
+    """
+    移动方块
+    :param x: 当前位置x
+    :param y: 当前位置y
+    :param direction: 移动方向 'l'向左 'r'向右
+    :param distance: 移动距离
+    :return: None
+    """
+    if direction == 'l':
+        print_block(x - distance, y)
+    elif direction == 'r':
+        print_block(x + distance, y)
+
+
+def spawn_block():
+    """新方块生成
+    """
+    # 方块挑选
+    b_type = 'IJLOSTZ'
+    tp = random.choice(b_type)
+    pick_block(tp)
+    # 出生点初始化
+    BLOCK_COORD['x'], BLOCK_COORD['y'] = BLOCK_SX, BLOCK_SY
+
+
+def clear_prev_block(prev_x, prev_y):
     pass
 
 
@@ -267,15 +323,13 @@ if __name__ == '__main__':
     tetris_init()
     time.sleep(1)
 
-    x, y = GAME_AREA_X, GAME_AREA_Y
     for i in 'IJLOTZS':
-        x = GAME_AREA_X
-        pick_block(i)
+        spawn_block()
+        print_block(BLOCK_COORD['x'], BLOCK_COORD['y'])
         for j in range(5):
-            print_block(x, y)
-            x += BLOCK_BITMAP[-2][-2][0] + 1
-            rotate_block()
-        y += BLOCK_BITMAP[-2][-2][1] + 2
+            move_block(BLOCK_COORD['x'], BLOCK_COORD['y'], 'r')
+            BLOCK_COORD['x'] += 1
+            time.sleep(0.5)
         time.sleep(0.1)
 
     print('\n')
